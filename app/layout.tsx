8a2394a98f403a3aa1cb42b5cd879533770a32fb
@amal7784404 <amal@trackerhero.com>
2026-05-14 23:49:40 +0800

    feat: initialize root layout with custom metadata, Google verification, and Geist font configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -15,9 +17,14 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "CherasRians Athletes Pro",
   description: "Transform Your Club's Potential.",
+
   icons: {
-  icon: "/favicon.png",
-},
+    icon: "/favicon.png",
+  },
+
+  verification: {
+    google: "6sXvA0417Q_PNwTSR25R4D1xLDGjTWLAPs0DlsO62Xs",
+  },
 };
 
 export default function RootLayout({
